7a05039b284c8751ba0d65e8bcc12d7e2373c4f6
Gabrielzzb <161638456+Gabrielzzb@users.noreply.github.com>
2025-05-06 16:27:44 -0300

    05/06

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ console.log(botoes);
 
 for(let i=0; i<botoes.length; i++){
 
+    for(let j = 0 <length; ii++){
+    botoes[j].classList.remove("ativo")
+    }
+
     botoes[i].onclick=function(){
         botoes[i].classList.add("ativo");
     }
